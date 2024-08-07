ba49e4983b46acfceee4c6e179f05ee2673d533a
Romeo509 <romeoameku503@gmail.com>
2024-08-07 22:53:49 +0000

    the constructor the creates a client to redis

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,16 @@
 // utils/db.js
 import { MongoClient } from 'mongodb';
-import { promisify } from 'util';
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    
+
     const uri = `mongodb://${host}:${port}`;
     this.client = new MongoClient(uri, { useUnifiedTopology: true });
     this.db = null;
-    
+
     // Connect to MongoDB
     this.client.connect().then(() => {
       this.db = this.client.db(database);
