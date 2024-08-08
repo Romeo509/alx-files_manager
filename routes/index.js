c1967dbcc8e001434b85a9b1b38db78c59209bf8
Romeo509 <romeoameku503@gmail.com>
2024-08-08 05:03:25 +0000

    the constructor the creates a client to redis

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,45 @@
-// routes/index.js
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = Router();
+const router = express.Router();
 
-// Existing routes
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+const setupRoutes = (app) => {
+  app.use('/', router);
 
-// New route for creating users
-router.post('/users', UsersController.postNew);
+  // App Routes
+  router.route('/status').get(AppController.getStatus);
 
-export default router;
+  router.route('/stats').get(AppController.getStats);
+
+  // User Routes
+  router.route('/users').post(UsersController.postNew);
+
+  router.route('/users/me').get(UsersController.getMe);
+
+  // Auth Routes
+  router.route('/connect').get(AuthController.getConnect);
+
+  router.route('/disconnect').get(AuthController.getDisconnect);
+
+  // File Routes
+  router.route('/files')
+    .post(FilesController.postUpload)
+    .get(FilesController.getIndex);
+
+  router.route('/files/:id')
+    .get(FilesController.getShow);
+
+  router.route('/files/:id/publish')
+    .put(FilesController.putPublish);
+
+  router.route('/files/:id/unpublish')
+    .put(FilesController.putUnpublish);
+
+  router.route('/files/:id/data')
+    .post(FilesController.getFile);
+};
+
+export default setupRoutes;
